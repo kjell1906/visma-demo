568974b3c2ff89fd2ce6c7832e606bfb6609cd3f
Kjell Dirdal <78570262+kjell1906@users.noreply.github.com>
2023-09-15 14:28:54 +0200

    Added color to textile records

--- src/main/java/com/example/vismademo/Application.java
+++ src/main/java/com/example/vismademo/Application.java
@@ -8,6 +8,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 @SpringBootApplication
 public class Application {
 
+	// Start
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
